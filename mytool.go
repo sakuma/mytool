cb0e2c15c550f63d1127ce4465267fe53d431dee
Naohiro Sakuma <nao.bear@gmail.com>
2014-07-19 22:43:07 +0900

    implement setup vim conf.

--- mytool.go
+++ mytool.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
 
 	"github.com/codegangsta/cli"
 )
@@ -13,9 +14,25 @@ func main() {
 	app.Usage = "print arguments"
 	app.Version = "0.0.1"
 	app.Action = func(c *cli.Context) {
-		fmt.Println("Number of arguments is", len(c.Args()))
-		fmt.Println("First argument is", c.Args()[0])
-		fmt.Println("Second argument is", c.Args()[1])
+		command := c.Args()[0]
+		fmt.Println("First argument is", command)
+		switch command {
+		case "install":
+			setupVim()
+		case "update":
+			fmt.Println("update...", command)
+		default:
+			fmt.Println("Unknown command: ", command)
+		}
 	}
 	app.Run(os.Args)
+}
+
+func setupVim() {
+	vimDirPath := path.Join(os.Getenv("HOME"), ".vim")
+	if _, err := os.Stat(vimDirPath); err == nil {
+		fmt.Println("exits!")
+	} else {
+		fmt.Println("not exits!")
+	}
 }
